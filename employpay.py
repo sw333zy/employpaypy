b5a12d9726a7ead182c51f81f14d09f385811dc8
J.Ridgeway <sw333zy@gmail.com>
2019-04-09 10:07:32 -0400

    Update employpay.py

--- employpay.py
+++ employpay.py
@@ -24,3 +24,21 @@ for employee in employees:
 
 
 print(employee['name'], weekpay)
+
+#ANOTHER WAY
+
+employee = [['001','Mary',15,40],['002','John',22,25],['003','Bob',35,4],
+            ['004','Mel',43,62],['005','Jen',17,33],['006','Sue',29,45],
+            ['007','Ken',40,36],['008','Dave',20,17],['009','Beth',37,37],
+            ['010','Ray',16.50,80]]
+for i in employee:
+    if i[3]>= 40:
+        pay = i[2]*i[3]
+        print(i[1],pay)
+        (i).append(pay)
+    else:
+        overtime = i[3]-40
+        pay = (i[2]*40)+ (i[2]*1.5*overtime)
+        print(i[1],pay)
+        (i).append(pay)
+
